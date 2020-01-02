4aac0d207ffdbcf42c5c3fa51f623947926fc02f
Mohit00 <naithanimnaithani@gmail.com>
2020-01-02 15:56:17 +0530

    with parent json file

---
+++ src/main/java/com/radius/sodalityUser/response/ParentGetJson.java
@@ -0,0 +1,14 @@
+package com.radius.sodalityUser.response;
+
+public class ParentGetJson {
+private String uuid;
+
+public String getUuid() {
+	return uuid;
+}
+
+public void setUuid(String uuid) {
+	this.uuid = uuid;
+}
+
+}
